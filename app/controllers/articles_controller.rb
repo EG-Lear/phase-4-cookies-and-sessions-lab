730f7f1fa4784b9a76a92763595b967f5e94cf3e
Garret <gwiats@gmail.com>
2021-07-08 23:33:13 -0400

    Done.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,13 +2,23 @@ class ArticlesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   def index
+    # session[:pageviews_remaining] ||= 3
+    # puts session[:pageviews_remaining]
+    # puts cookies
     articles = Article.all.includes(:user).order(created_at: :desc)
     render json: articles, each_serializer: ArticleListSerializer
   end
 
   def show
-    article = Article.find(params[:id])
-    render json: article
+    session[:pageviews_remaining] ||= 3
+    if session[:pageviews_remaining] > 1
+      session[:pageviews_remaining] -= 1
+      article = Article.find(params[:id])
+      render json: article
+    else
+      session[:pageviews_remaining] -= 1
+      render json: { error: "Maximum pageview limit reached" }, status: :unauthorized
+    end
   end
 
   private
